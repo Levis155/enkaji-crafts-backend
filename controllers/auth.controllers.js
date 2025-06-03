6da636058d63c0d62de07084f7d2a3c74050bf7b
Levis155 <levismbui@gmail.com>
2025-06-03 12:11:13 +0300

    set up login endpoint

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,5 +1,6 @@
-import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaClient } from "@prisma/client"
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const client = new PrismaClient()
 
@@ -18,7 +19,42 @@ export const register = async(req, res) => {
 
         res.status(201).json({message:"User registered successfully."})
     } catch(e) {
-        res.status(500).json({message: "Something went wrong. Please Try again."});
+        res.status(500).json({message: "Something went wrong. Please try again."});
         console.log(e)
+    }
+}
+
+export const login = async (req, res) => {
+    try{
+        const {emailAddress, password} = req.body;
+
+        const user = await client.user.findFirst({where: {
+            emailAddress
+        }});
+
+        if (!user) {
+            return res.status(401).json({message: "Wrong login credentials."})
+        };
+
+        const theyMatch = await bcrypt.compare(password, user.password);
+
+        if(!theyMatch) {
+            return res.status(401).json({message: "Wrong login credentials."})
+        };
+
+        const jwtPayload = {
+            id: user.id,
+            fullName: user.fullName
+        };
+
+        const token = jwt.sign(jwtPayload, process.env.JWT_SECRET_KEY);
+
+        res.status(200).cookie("enkajiAuthToken", token).json({
+            fullName: user.fullName,
+            emailAddress: user.emailAddress,
+            phoneNumber: user.phoneNumber
+        })
+    } catch(e) {
+        res.status(500).json({message: "Something went wrong. Please try again."})
     }
 }
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,11 @@
 import {Router} from "express";
-import { register } from "../controllers/auth.controllers.js";
+import { register, login } from "../controllers/auth.controllers.js";
 import validateEmail from "../middleware/validateEmail.js";
 
 
 const router = Router();
 
-router.route("/register").post(validateEmail, register)
+router.route("/register").post(validateEmail, register);
+router.route("/login").post(login)
 
 export default router;
