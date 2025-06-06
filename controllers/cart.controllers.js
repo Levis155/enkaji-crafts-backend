f9e3f377404ff68b8a82e4ada15f063eb824935d
Levis155 <levismbui@gmail.com>
2025-06-06 11:04:20 +0300

    implement add to cart endpoint

--- controllers/cart.controllers.js
+++ controllers/cart.controllers.js
@@ -1,3 +1,20 @@
-export const addToCart = (req, res) => {
-    res.send("adding item to cart.")
-}
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+export const addToCart = async (req, res) => {
+  try {
+    const { productId, userId } = req.body;
+
+    const newCartItem = await client.cart.create({
+      data: {
+        productId,
+        userId,
+      },
+    });
+
+    res.status(201).json({ message: "Product added to cart successfully" });
+  } catch (e) {
+    res.status(500).json({ message: "Something went wrong." });
+  }
+};
---
+++ prisma/migrations/20250606071725_update_prisma_schema/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the `cart` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "cart" DROP CONSTRAINT "cart_product_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "cart" DROP CONSTRAINT "cart_user_id_fkey";
+
+-- DropTable
+DROP TABLE "cart";
