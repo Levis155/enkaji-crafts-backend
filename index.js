ab2b7beea00967b0299026b4967723a9352b4406
Levis155 <levismbui@gmail.com>
2025-06-25 21:14:14 +0300

    modify user model

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(
       "http://localhost:5173",
       "https://red-dune.vercel.app",
       "http://localhost:5174",
+      "http://localhost:5175",
       "https://enkaji-crafts-admin-panel.vercel.app"
     ],
     methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
